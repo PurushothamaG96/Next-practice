bcca9bfc8d36df23de5e38bf2c36b4f10f8b4bfb
PurushothamaG96 <purushothamachar96@gmail.com>
2023-09-03 09:16:06 +0530

    next@13.2.4 Wikipedia blog site

---
+++ next05/type.d.ts
@@ -0,0 +1,18 @@
+//wikipedia resource type code
+
+type Result = {
+  pageid: string;
+  title: string;
+  extract: string;
+  thumbnail?: {
+    source: string;
+    width: number;
+    height: number;
+  };
+};
+
+type SearchResult = {
+  query?: {
+    pages?: Result[];
+  };
+};
